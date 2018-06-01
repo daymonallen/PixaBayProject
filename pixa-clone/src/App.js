82d4aa56f16346a3e3bf65127241724d507397be
Beaux <daymon.allen@gmail.com>
2018-05-31 20:23:32 -0500

    Create File System and strip React-App defaults

--- pixa-clone/src/App.js
+++ pixa-clone/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Photo search application
-        </p>
+      Hello Bitches
       </div>
     );
   }
